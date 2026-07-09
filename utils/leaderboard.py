ed301927c21af9204eac7c8af0cc96eaae758db3
esatyasar726-creator <esatyasar726@gmail.com>
2026-07-09 20:32:47 +0300

    Create leaderboard.py

---
+++ utils/leaderboard.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+FILE = "data/leaderboard.json"
+
+
+def load_scores():
+    if not os.path.exists(FILE):
+        with open(FILE, "w") as f:
+            json.dump({}, f)
+
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+
+def save_scores(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def add_points(user_id, username, points):
+    data = load_scores()
+
+    uid = str(user_id)
+
+    if uid not in data:
+        data[uid] = {
+            "username": username,
+            "score": 0
+        }
+
+    data[uid]["username"] = username
+    data[uid]["score"] += points
+
+    save_scores(data)
+
+
+def get_top():
+    data = load_scores()
+
+    ranking = sorted(
+        data.values(),
+        key=lambda x: x["score"],
+        reverse=True
+    )
+
+    return ranking[:10]
