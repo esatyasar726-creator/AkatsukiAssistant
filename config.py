fa9fa1c52a7470857f2924384d6c8b89a4cd47a0
esatyasar726-creator <esatyasar726@gmail.com>
2026-07-09 19:10:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Banner referans günü
+# 9 Temmuz 2026 = Day 299
+REFERENCE_DAY = 299
+
+# YYYY, MM, DD
+REFERENCE_DATE = (2026, 7, 9)
+
+MAX_BANNER_DAY = 403
+
+# Quiz
+QUIZ_CORRECT = 5
+QUIZ_WRONG = -3
+
+# Daily Question
+DAILY_CORRECT = 15
+DAILY_WRONG = -5
+
+# Admin kullanıcı adları
+ADMINS = [
+    "Klaus",
+    "Asta",
+    "YurtSever",
+    "Baba Yaga",
+    "Jaco Tenma",
+    "Mutoh"
+]
